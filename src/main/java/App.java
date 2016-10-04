4ed6a10cc1c068fc693cbbdea7dd114e5196ad88
tiki84626 <andymalkin@gmail.com>
2016-10-04 04:13:50 -0700

    add menu to root directory stylists and clients

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,11 +4,42 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 import org.sql2o.*;
+import java.sql.Date;
 
 public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      model.put("hairsalons", HairSalon.all());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylists", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/stylists.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylists/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/stylist-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylists/:stylistId", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      int stylistId = Integer.parseInt(request.params(":stylistId"));
+      Stylist stylist = Stylist.find(stylistId);
+      model.put("stylist", stylist);
+      model.put("clients", stylist.getClients());
+      model.put("template", "templates/stylist.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
 
   }
