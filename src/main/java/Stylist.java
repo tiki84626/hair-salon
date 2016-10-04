e6eb1fbdc85d25dbc085566e16221af951f82ce6
tiki84626 <andymalkin@gmail.com>
2016-10-03 22:20:02 -0700

    add get tests for stylist

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -95,4 +95,13 @@ public class Stylist{
     }
   }
 
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM stylists WHERE id = :id;";
+      con.createQuery(sql)
+      .addParameter("id", id)
+      .executeUpdate();
+    }
+  }
+
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -1,6 +1,7 @@
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.*;
 
 public class StylistTest {
   @Rule
@@ -8,8 +9,32 @@ public class StylistTest {
 
   @Test
   public void stylist_instantiatesCorrectly_true() {
-    Stylist testStylist = new Stylist("Susan Smith", "susansmith@gmail.com", "123-456-7890", 1);
+    Stylist testStylist = new Stylist("Susan", "susan@test.com", "123-456-7890", 1);
     assertEquals(true, testStylist instanceof Stylist);
+  }
+
+  @Test
+  public void getName_grabsNameFromStylistObject_String() {
+    Stylist testStylist = new Stylist("Susan", "susan@test.com", "123-456-7890", 1);
+    assertEquals("Susan", testStylist.getName());
+  }
+
+  @Test
+  public void getPhoneNumber_grabsPhoneNumberFromStylistObject_String() {
+    Stylist testStylist = new Stylist("Susan", "susan@test.com", "123-456-7890", 1);
+    assertEquals("123-456-7890", testStylist.getPhoneNumber());
+  }
+
+  @Test
+  public void getEmail_grabsEmailFromStylistObject_String() {
+    Stylist testStylist = new Stylist("Susan", "susan@test.com", "123-456-7890", 1);
+    assertEquals("susan@test.com", testStylist.getEmail());
+  }
+
+  @Test
+  public void getHairSalonId_grabsHairSalonIdFromStylistObject_String() {
+    Stylist testStylist = new Stylist("Susan", "susan@test.com", "123-456-7890", 1);
+    assertEquals(1, testStylist.getHairSalonId());
   }
 
 }
